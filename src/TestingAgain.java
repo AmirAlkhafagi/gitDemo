62ef9674945dea8bd3250a1bd85dc52c47f9db04
AmirAlkhafagi <alkhafagi.amir@gmail.com>
2020-08-01 11:37:25 -0500

    Update TestingAgain.java

--- src/TestingAgain.java
+++ src/TestingAgain.java
@@ -2,6 +2,10 @@ public class TestingAgain {
     public static void main(String[] args) {
         int a = 5;
         int b=a;
-        System.out.println(b);
+        System.out.println(b + " git init");
+
+        String name = "git add .";
+        String description = " then, git commit -m\"...\"";
+        System.out.println(name+description);
     }
 }
